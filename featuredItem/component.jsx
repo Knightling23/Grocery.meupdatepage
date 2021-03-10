f7a6740e48a5d2772552ba9a09eff31bf6bf1097
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-03-10 17:18:10 -0500

    Delete featuredItem directory

--- featuredItem/component.jsx
+++
@@ -1,7 +0,0 @@
-import hero from './assets/hero.jpg'
-
-export const FeaturedItem = ({ updatePage }) => {
-  return <>
-    <img className="logoImage" src={hero} alt="Hero Image"/>
-  </>
-}
--- featuredItem/index.js
+++
@@ -1,4 +0,0 @@
-import './styles.css';
-import { FeaturedItem } from './component';
-
-export { FeaturedItem };
